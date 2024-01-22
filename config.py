f0e924706ad8d3f382af31305229c8826fcd9cae
Steve A <steveagboola@gmail.com>
2024-01-21 22:53:05 -0700

    ready

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Get the base directory of this folder
 basedir = os.path.abspath(os.path.dirname(__file__))
-# "C:\\Users\\bstan\\Documents\\codingtemple-kekambas-137\\week6\\flask_api"
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
